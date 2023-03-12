3edf776c6a1b891ae89dc00ebb53d1a252f7d817
qoisnizarmahendra <nizarmahendra919@gmail.com>
2023-03-12 10:07:09 +0700

    perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
-      padding: '16px'
+      padding: '16px',
     },
     extend: {
       colors: {
@@ -14,8 +15,8 @@ module.exports = {
       },
       screens: {
         '2xl': '1320px',
-      }
+      },
     },
   },
   plugins: [],
-}
+};
